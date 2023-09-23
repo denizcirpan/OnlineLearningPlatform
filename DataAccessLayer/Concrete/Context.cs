894f157b5a2bdfee3d8fa8f91de19554347e7cca
Deniz Çırpan <119450214+denizcirpan@users.noreply.github.com>
2023-09-23 22:09:57 +0300

    Data Access Layer Completed

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<CourseResource> CourseResources { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public DbSet<StudentCourseAssignment> StudentCourseAssignments { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<CourseEnrollment> CourseEnrollments { get; set; }
+    }
+}
